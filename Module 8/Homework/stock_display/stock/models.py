0fefc25d5c805e07b3620a791766c4db5bf30293
Eric Zorn <goldencatch93@gmail.com>
2018-02-15 19:49:46 -0500

    Beginning Django Stocks Module 8

---
+++ Module 8/Homework/stock_display/stock/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class Stock(models.Model):
+    id = models.IntegerField(primary_key=True)
+    symbol = models.CharField(max_length=10)
+    shares = models.IntegerField()
+    purchase_price = models.DecimalField(decimal_places=1, max_digits=20)
+    current_price = models.DecimalField(decimal_places=1, max_digits=20)
+    purchase_date = models.CharField(max_length=9)
+
+    def __str__(self):
+        return self.symbol + " - " + str(self.shares)
---
+++ Module 8/Homework/stock_display/stock/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render, HttpResponse
+from django.views.generic import RedirectView
+from stock.models import Stock
+import sqlite3
+
+
+# Create your views here.
+def Home(request):
+    title = "ICT 4370 Stock Information"
+    investor_query = "SELECT full_name FROM investors"
+    stock_query = "SELECT * FROM stocks"
+
+    conn = sqlite3.connect('stock_info.db')
+    cursor = conn.cursor()
+
+    cursor.execute(investor_query)
+    return_data_investor = cursor.fetchall()
+
+    cursor.execute(stock_query)
+    return_data_stock = cursor.fetchall()
+
+    context = {
+        "investor_one": return_data_investor[0][0],
+        "investor_two": return_data_investor[1][0],
+        "google_stock": return_data_stock[0],
+        "msft_stock": return_data_stock[1],
+        "rds_a_stock": return_data_stock[2],
+        "aig_stock": return_data_stock[3],
+        "fb_stock": return_data_stock[4],
+        "m_stock": return_data_stock[5],
+        "f_stock": return_data_stock[6],
+        "ibm_stock": return_data_stock[7],
+        "appl_stock": return_data_stock[8],
+        "title": title,
+    }
+
+    conn.close()
+
+    return render(request, 'stock/index.html', context)
+
+
+# Bonds View
+def Bonds(request):
+    title = "ICT 4370 Stock Information"
+    investor_query = "SELECT full_name FROM investors"
+    bond_query = "SELECT * FROM bonds"
+
+    conn = sqlite3.connect('stock_info.db')
+    cursor = conn.cursor()
+
+    cursor.execute(investor_query)
+    return_data_investor = cursor.fetchall()
+
+    cursor.execute(bond_query)
+    return_data_bond = cursor.fetchall()
+
+    context = {
+        "investor_one": return_data_investor[0][0],
+        "investor_two": return_data_investor[1][0],
+        "bond_one": return_data_bond[0],
+        "bond_one_purchase_date": "8/1/2017",
+        "bond_one_purchase_price": 100.02,
+        "bond_one_current_price": 100.05,
+        "title": title,
+    }
+
+    conn.close()
+
+    return render(request, 'bonds/bonds.html', context)
